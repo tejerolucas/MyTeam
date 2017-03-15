2a8c5ae058728ced95f5980ac4ed5de448ade4f6
Lucas Tejero <tejerolucas@gmail.com>
2017-03-15 19:43:25 -0300

    Orden de Proyecto

---
+++ Assets/Scripts/Signin.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using Firebase.Database;
+
+public class Signin : MonoBehaviour {
+	public string email;
+	public string password;
+
+	public void SignIn(){
+		Debug.LogWarning ("SignIn");	
+		UserAuth.instance.CreateUserWithEmail (email, password, "nombre");	
+	}
+
+		public void changeemail(string st){
+				email = st;
+		}
+
+		public void changepassword(string st){
+				password = st;
+		}
+}
---
+++ Assets/Scripts/puntajemanager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Firebase.Database;
+
+public class puntajemanager : MonoBehaviour {
+		public GameObject button;
+		public Image _image;
+		public Text _name;
+		public Text _position;
+		public togglelist estrellas;
+		public togglelist corazones;
+	public string url;
+
+	void Start () {
+		button.SetActive(false);
+	}
+	
+	public void updatepuntaje () {
+		if(estrellas.ready&&corazones.ready){
+			button.SetActive(true);
+		}
+	}
+
+	public void SetImage(Sprite sprite,string ur){
+		_image.sprite=sprite;	
+		url = ur;
+	}
+	public void SetName(string nombre){
+		_name.text=nombre;
+	}
+	public void SetPosition(string posicion){
+		_position.text=posicion;
+	}
+
+
+	
+	public void Reset(){
+				button.SetActive(false);
+				corazones.Reset();
+				estrellas.Reset();
+	}
+}
