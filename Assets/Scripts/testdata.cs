2547bfd63f206dbc823973751cb43722ed1b5c3d
Lucas Tejero <tejerolucas@gmail.com>
2017-03-15 19:54:56 -0300

    Revert "Revert "Orden de Proyecto""
    This reverts commit 1ae89cb32dd4406a130015905a7d9c79be2f97aa.

---
+++ Assets/Scripts/testdata.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Firebase.Database;
+using Firebase;
+using Firebase.Unity.Editor;
+
+public class testdata : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		FirebaseApp app = FirebaseApp.DefaultInstance;
+		app.SetEditorDatabaseUrl ("https://soccerapp-5d7ac.firebaseio.com/");
+		if (app.Options.DatabaseUrl != null) {
+		app.SetEditorDatabaseUrl (app.Options.DatabaseUrl);
+		}
+
+			FirebaseDatabase.DefaultInstance.GetReference("Jugadores/ShWNahpi3bMdotWdtWRgTHRnFt12/amonestaciones").ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+	      Debug.Log(e2.Snapshot.Value);
+	    };
+
+	}
+}
