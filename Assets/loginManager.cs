1ae89cb32dd4406a130015905a7d9c79be2f97aa
Lucas Tejero <tejerolucas@gmail.com>
2017-03-15 19:48:37 -0300

    Revert "Orden de Proyecto"
    This reverts commit 2a8c5ae058728ced95f5980ac4ed5de448ade4f6.

---
+++ Assets/loginManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loginManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void SignIn(){
+		
+	}
+}
