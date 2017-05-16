4a16d654697edd3ca81915826eeb9f58b657c62f
tejerolucas <tejerolucas@gmail.com>
2017-05-16 18:01:31 -0300

    admin

--- Assets/Scripts/Admin.cs
+++ Assets/Scripts/Admin.cs
@@ -2,18 +2,43 @@
 using System.Collections;
 
 public class Admin : MonoBehaviour {
-	public etermaxplayers eterplayers;
-	public GameObject amonestarpopup;
-	public void CrearEvento(){
+	public int cantidad;
+	public int cantidad2;
+	public string estado;
 
+	public void CrearJugadores(){
+		WWW www = new WWW ("https://us-central1-soccerapp-5d7ac.cloudfunctions.net/CreateTesterPlayers?cantidad=" + cantidad.ToString ());
 	}
 
-	public void GenerarEquipo(){
-
+	public void AgregarJugadores(){
+		WWW www = new WWW ("https://us-central1-soccerapp-5d7ac.cloudfunctions.net/AddPlayerstoEvent?cantidad=" + cantidad2.ToString ());
 	}
 
-	public void Amonestar ()
+	public void BorrarJugadores ()
 	{
-		eterplayers.SetPopUp (amonestarpopup);
+		WWW www = new WWW ("https://us-central1-soccerapp-5d7ac.cloudfunctions.net/ClearTesterUsers");
+	}
+
+	public void ChangeState ()
+	{
+		WWW www = new WWW ("https://us-central1-soccerapp-5d7ac.cloudfunctions.net/ChangeEventState?estado="+estado);
+	}
+
+	public void updatecantidad(string num){
+		int numero = 0;
+		if(int.TryParse(num,out numero)){
+			cantidad = numero;
+		}
+	}
+
+	public void updatecantidad2(string num){
+		int numero = 0;
+		if(int.TryParse(num,out numero)){
+			cantidad2 = numero;
+		}
+	}
+
+	public void updateEstado(string num){
+		estado = num;
 	}
 }
