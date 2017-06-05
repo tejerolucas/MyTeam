4fbe34a6535b5805c76528b1e67dd7f384cb7fdb
tejerolucas <tejerolucas@gmail.com>
2017-06-05 14:06:32 -0300

    estrellas en perfil

--- Assets/Scripts/Perfil.cs
+++ Assets/Scripts/Perfil.cs
@@ -5,13 +5,17 @@ using Firebase.Database;
 using Firebase;
 using Firebase.Unity.Editor;
 using MaterialUI;
+using System;
+using System.Globalization;
 
 public class Perfil : MonoBehaviour {
 	public Text nombre;
 	public Text puesto;
 	public Text amonestaciones;
+	public Text valoraciones;
 	public Image foto;
-	public MaterialSwitch notiswitch;
+	private string userid;
+	public Image mask;
 	// Use this for initialization
 	void Start ()
 	{
@@ -26,26 +30,35 @@ public class Perfil : MonoBehaviour {
 		if (Resources.Load ("Fotos/" + UserAuth.instance._userfilename) != null) {
 			foto.sprite = Resources.Load<Sprite> ("Fotos/" + UserAuth.instance._userfilename);
 		}
-		if (PlayerPrefs.GetInt ("Notifications") == 1) {
-			notiswitch.TurnOn ();
-		} else {
-			notiswitch.TurnOff();
-		}
 	}
 
 	void OnEnable ()
 	{
-		if (UserAuth.instance.user != null)
-		{
-			FirebaseDatabase.DefaultInstance.GetReference("Jugadores/"+UserAuth.instance.user.UserId).ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+		FirebaseApp app = FirebaseApp.DefaultInstance;
+		app.SetEditorDatabaseUrl ("https://soccerapp-5d7ac.firebaseio.com/");
+		if (app.Options.DatabaseUrl != null) {
+			app.SetEditorDatabaseUrl (app.Options.DatabaseUrl);
+		}
+
+
+		#if UNITY_EDITOR
+		userid="editor";
+		#else
+		userid=UserAuth.instance.user.UserId;
+		#endif
+
+			FirebaseDatabase.DefaultInstance.GetReference("Jugadores/"+userid).ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
 				amonestaciones.text =e2.Snapshot.Child("amonestaciones").Value.ToString();
 				nombre.text=e2.Snapshot.Child("nombre").Value.ToString();
 				puesto.text=e2.Snapshot.Child("puesto").Value.ToString();
 				if (Resources.Load ("Fotos/"+e2.Snapshot.Child("filename").Value.ToString()) != null) {
 					foto.sprite = Resources.Load<Sprite> ("Fotos/" + e2.Snapshot.Child("filename").Value.ToString());
 				}
-	    };
-		}
+				valoraciones.text=e2.Snapshot.Child("Estrella").Value.ToString()+"\n"+e2.Snapshot.Child("valoracionesrecibidas").ChildrenCount.ToString();
+			Debug.Log(float.Parse(e2.Snapshot.Child("Estrella").Value.ToString(),CultureInfo.InvariantCulture.NumberFormat));
+			mask.fillAmount=float.Parse(e2.Snapshot.Child("Estrella").Value.ToString())/5.0f;
+
+	   	 	};
 
 	}
 
